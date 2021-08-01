5b217110b66f6bf31f030bfe388c67454424b57b
Manuel Stefan Christopher <manuelstefan150@gmail.com>
2021-08-02 01:06:44 +0700

    Created the 3rd webpage

--- data.py
+++ data.py
@@ -5,11 +5,13 @@ import datetime
 start = datetime.datetime(2021,7,1)
 end = datetime.datetime(2021,7,29)
 try:
-    etfs = ['VCN', 'VUN', 'VIU', 'VEE']
-    bonds = ['VBU', 'VBG']
+    etfs = ['VCN.TO', 'VUN.TO', 'VIU.TO', 'VEE.TO']
+    bonds = ['VBU.TO', 'VBG.TO']
 
-    etf = yf.download('SPY IVV QQQ',start=start, end=end, progress=False)
+    etf = yf.download(etfs,start=start, end=end, progress=False)["Close"]
+    bond = yf.download(bonds,start=start, end=end, progress=False)["Close"]
 except Exception:
     None
 
-print(etf)
+#print(etf)
+#print(bond)
--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 from flask import request,Flask,app,jsonify,render_template
 
-from data import etf
+from data import etf, bond
 
 app = Flask(__name__)
 
 
 
 @app.route('/',methods=['GET','POST'])
-
 def init():
     if request.method == 'POST':
         investment = request.form.get('investment')
         time = request.form.get('time')
         risk = request.form.get('risk')
         print(investment,time,risk)
-        data=[{"symbol":'VCN',"name":"Vanguard FTSE Canada All Cap Index ETF","asset":"ETFs","price":41.02,"_shares":4},
-        {"symbol":'VUN',"name":"Vanguard U.S. Total Market Index ETF","asset":"ETFs","price":76.87,"_shares":4},
-        {"symbol":'VIU',"name":"Vanguard FTSE Developed All Cap Ex North America Index ETF","asset":"ETFs","price":32.37,"_shares":4}]
+        data=[{"symbol":'VCN.TO',"name":"Vanguard FTSE Canada All Cap Index ETF","asset":"ETFs","price":41.02,"_shares":4},
+        {"symbol":'VUN.TO',"name":"Vanguard U.S. Total Market Index ETF","asset":"ETFs","price":76.87,"_shares":4},
+        {"symbol":'VIU.TO',"name":"Vanguard FTSE Developed All Cap Ex North America Index ETF","asset":"ETFs","price":32.37,"_shares":4}]
         return render_template("suggest.html",data=data)
 
     else:
         return render_template("input.html")
+
+@app.route('/graph/v')
+def graph():
+    return render_template('graph.html')
 
 app.run(host='0.0.0.0',port=5000,debug=True)
 
