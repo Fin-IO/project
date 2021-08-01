baaabf5b6ff8660ee4222cfd58512c3a3bd9040e
Manuel Stefan Christopher <manuelstefan150@gmail.com>
2021-08-01 16:23:49 +0700

    Added the ETFs and bonds

--- data.py
+++ data.py
@@ -5,9 +5,11 @@ import datetime
 start = datetime.datetime(2021,7,1)
 end = datetime.datetime(2021,7,29)
 try:
-    stock = []
-    stock = yf.download('FB',start=start, end=end, progress=False)
+    etfs = ['VCN', 'VUN', 'VIU', 'VEE']
+    bonds = ['VBU', 'VBG']
+
+    etf = yf.download('SPY IVV QQQ',start=start, end=end, progress=False)
 except Exception:
     None
 
-print(stock)
+print(etf)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import request,Flask,app,jsonify,render_template
 
+from data import etf
 
 app = Flask(__name__)
 
@@ -13,7 +14,9 @@ def init():
         time = request.form.get('time')
         risk = request.form.get('risk')
         print(investment,time,risk)
-        data=[{"symbol":'FB',"name":"Facebook","asset":"equity","price":45,"_shares":4},{"symbol":'FB',"name":"Facebook","asset":"equity","price":45,"_shares":4},{"symbol":'FB',"name":"Facebook","asset":"equity","price":45,"_shares":4}]
+        data=[{"symbol":'VCN',"name":"Vanguard FTSE Canada All Cap Index ETF","asset":"ETFs","price":41.02,"_shares":4},
+        {"symbol":'VUN',"name":"Vanguard U.S. Total Market Index ETF","asset":"ETFs","price":76.87,"_shares":4},
+        {"symbol":'VIU',"name":"Vanguard FTSE Developed All Cap Ex North America Index ETF","asset":"ETFs","price":32.37,"_shares":4}]
         return render_template("suggest.html",data=data)
 
     else:
